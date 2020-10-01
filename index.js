00e1849fa6312f6d0fc4cb385d39b48577bfce64
unknown <talhak597@gmail.com>
2020-10-01 12:25:52 -0600

    This commit has changes  that represent middleware, enviroment variables, config module, etc

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+const config = require("config");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const Joi = require("joi");
+const logger = require("./logger");
 const express = require("express");
 const app = express();
 
+//console.log(`NODE_ENV: ${[process.env.NODE_ENV]}`);
+//console.log(`app: ${app.get("env")}`);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+
+//Configuration
+console.log("Applicatoin Name: " + config.get("name"));
+console.log("Mail Server: " + config.get("mail.host"));
+console.log("Mail password: " + config.get("mail.password"));
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("morgan enabled...");
+}
+
+app.use(logger);
+
+app.use(function (req, res, next) {
+  console.log("Authenticating...");
+  next();
+});
 
 const courses = [
   { id: 1, name: "course1" },
@@ -23,10 +50,7 @@ app.get("/api/courses", (req, res) => {
 
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -41,19 +65,29 @@ app.put("/api/courses/:id", (req, res) => {
   // If not existing, return 404
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given ID was not found!");
+    return res.status(404).send("The course with the given ID was not found!");
 
   //const result = validateCourse(req.body);
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   // Update course
   course.name = req.body.name;
 
   // return the update course to the client
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  //Look up the course
+  // Not existing, 404 error
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found!");
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
   res.send(course);
 });
 
@@ -68,7 +102,7 @@ function validateCourse(course) {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given ID was not found");
+    return res.status(404).send("The course with the given ID was not found");
   res.send(course);
 });
 
